4d5f7cd4cbff641c4fd263976ffaae60ee932570
Szymon Krzysztyniak <szymon.krzysztyniak@cognifide.com>
2015-12-15 11:25:13 +0100

    RND-394 check if in code http literal is used

--- src/main/java/com/cognifide/aemrules/extensions/CheckListRegistrar.java
+++ src/main/java/com/cognifide/aemrules/extensions/CheckListRegistrar.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import com.cognifide.aemrules.checks.HttpConstantCheck;
 import org.sonar.plugins.java.api.CheckRegistrar;
 import org.sonar.plugins.java.api.JavaCheck;
 
@@ -30,7 +31,8 @@ public class CheckListRegistrar implements CheckRegistrar {
 				InjectorTryWithResourcesCheck.class,
 				ResourceResolverShouldBeClosed.class,
 				SessionShouldBeLoggedOut.class,
-				PreferSlingServletAnnotation.class
+				PreferSlingServletAnnotation.class,
+				HttpConstantCheck.class
 		};
 		// @formatter:on
 	}
