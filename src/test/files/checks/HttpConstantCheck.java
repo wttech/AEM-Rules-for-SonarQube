dbca87cb3b638d6ef296eb20a4638a354c915478
Lukasz Marczak <lukasz.marczak@cognifide.com>
2016-01-21 13:02:57 +0100

    RND-394 refactor: remove duplicated text as stated in code review

--- src/test/files/checks/HttpConstantCheck.java
+++ src/test/files/checks/HttpConstantCheck.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HttpConstantCheck {
 
-    private String httpLiteral = "aaa"; // Noncompliant {{Using http literal hardcoded makes it difficult to switch to https later on.}}
+    private String httpLiteral = "http"; // Noncompliant {{Using http literal hardcoded makes it difficult to switch to https later on.}}
 
     protected void protectedMethod(String test) {
         String httpProtocol = "http"; // Noncompliant
