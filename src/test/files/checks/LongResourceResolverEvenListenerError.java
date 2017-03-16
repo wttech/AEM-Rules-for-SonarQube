549420a1c5fd736e9c42dbec0d21fb5eb5f473a6
Dariusz Blicharski <dariusz.blicharski@cognifide.com>
2017-03-16 14:16:44 +0100

    Modified AEM-6 and AEM-7 rules to be aware of the case when resourceResolver or Session can be closed in @Deactivate methods instead of closing right away. Second part of this commit is pom.xml modification which was done to fix behaviour of JavaCheckVerifier.verifyNoIssue method (which previously did not return correct results because of lack of jar files in target/test-jars folder)

---
+++ src/test/files/checks/LongResourceResolverEvenListenerError.java
@@ -0,0 +1,65 @@
+/*-
+ * #%L
+ * AEM Rules for SonarQube
+ * %%
+ * Copyright (C) 2015 Cognifide Limited
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.example;
+
+import java.util.Map;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.observation.EventIterator;
+import javax.jcr.observation.EventListener;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.jcr.api.SlingRepository;
+
+@Component
+public class LongResourceResolverEvenListenerError implements EventListener {
+
+	private ResourceResolver resolver; // Noncompliant {{ResourceResolver should be closed in finally block.}}
+
+	@Reference
+	private SlingRepository repository;
+
+	@Reference
+	private ResourceResolverFactory resourceResolverFactory;
+
+	@Activate
+	public void activate(final Map<String, String> config) throws RepositoryException, LoginException {
+		resolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+	}
+
+	@Deactivate
+	public void deactivate(final Map<String, String> config) throws RepositoryException {
+		try {
+			final Session session = resolver.adaptTo(Session.class);
+		} finally {
+		}
+	}
+
+	@Override
+	public void onEvent(final EventIterator events) {
+	}
+}
