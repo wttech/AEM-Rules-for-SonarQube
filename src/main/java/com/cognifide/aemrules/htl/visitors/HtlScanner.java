81d6dd1200c78664c98347a4917714b009082ed9
Piotr Wilczynski <piotr.wilczynski@cognifide.com>
2018-10-31 14:16:00 +0100

    issue-63 removed unused import

--- src/main/java/com/cognifide/aemrules/htl/visitors/HtlScanner.java
+++ src/main/java/com/cognifide/aemrules/htl/visitors/HtlScanner.java
@@ -20,7 +20,6 @@
 package com.cognifide.aemrules.htl.visitors;
 
 import com.google.common.collect.Lists;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
