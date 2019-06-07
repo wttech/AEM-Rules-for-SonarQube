a35b841f1043e92584fe22baf6a0c328b34107c6
Mateusz Bartkowiak <mateusz.bartkowiak@cognifide.com>
2019-06-07 09:15:24 +0200

    #115 Using startElement() new instead of htlExpression().

--- src/main/java/com/cognifide/aemrules/htl/checks/ExplicitNamesInLoopsCheck.java
+++ src/main/java/com/cognifide/aemrules/htl/checks/ExplicitNamesInLoopsCheck.java
@@ -3,10 +3,8 @@ package com.cognifide.aemrules.htl.checks;
 import com.cognifide.aemrules.metadata.Metadata;
 import com.cognifide.aemrules.tag.Tags;
 import com.cognifide.aemrules.version.AemVersion;
-import org.apache.sling.scripting.sightly.compiler.expression.Expression;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
-import org.sonar.plugins.html.node.Node;
 import org.sonar.plugins.html.node.TagNode;
 
 @Rule(
@@ -30,8 +28,8 @@ public class ExplicitNamesInLoopsCheck extends AbstractHtlCheck {
   static final String RULE_MESSAGE = "Use explicit names in loops";
 
   @Override
-  public void htlExpression(Expression expression, Node node) {
-    if (node instanceof TagNode && (isSlyList((TagNode) node) || isSlyRepeat((TagNode) node))) {
+  public void startElement(TagNode node) {
+    if (isSlyList(node) || isSlyRepeat(node)) {
       createViolation(node.getStartLinePosition(), RULE_MESSAGE);
     }
   }
