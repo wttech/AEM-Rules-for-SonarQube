a69ffd25116d6bd7c13309bdcd9e8f1df906e7a9
GulderBone <mateusz.bartkowiak@cognifide.com>
2020-03-23 09:45:38 +0100

    issue-180 | Java parser updated.

--- src/test/java/com/cognifide/aemrules/matcher/MethodMatcherTest.java
+++ src/test/java/com/cognifide/aemrules/matcher/MethodMatcherTest.java
@@ -19,26 +19,18 @@
  */
 package com.cognifide.aemrules.matcher;
 
-import static org.hamcrest.CoreMatchers.is;
-
-import com.sonar.sslr.api.typed.ActionParser;
-import java.util.Collections;
+import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
-import org.sonar.java.ast.parser.JavaParser;
-import org.sonar.java.bytecode.loader.SquidClassLoader;
-import org.sonar.java.resolve.SemanticModel;
-import org.sonar.plugins.java.api.tree.ClassTree;
-import org.sonar.plugins.java.api.tree.CompilationUnitTree;
-import org.sonar.plugins.java.api.tree.ExpressionStatementTree;
-import org.sonar.plugins.java.api.tree.MethodInvocationTree;
-import org.sonar.plugins.java.api.tree.MethodTree;
-import org.sonar.plugins.java.api.tree.StatementTree;
-import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.java.model.JParser;
+import org.sonar.plugins.java.api.tree.*;
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class MethodMatcherTest {
-
-    private static final ActionParser<Tree> JAVA_PARSER = JavaParser.createParser();
 
     private static final String CODE_TO_PARSE_METHOD_SELECT_KIND_IDENTIFIER = "package com.cognifide.test; "
         + "class MyClass1 {}"
@@ -148,11 +140,15 @@ public class MethodMatcherTest {
     }
 
     private void givenMethodInvocationTree(String codeToParse) {
-        CompilationUnitTree compilationUnitTree = (CompilationUnitTree) JAVA_PARSER.parse(codeToParse);
-        SemanticModel.createFor(compilationUnitTree, new SquidClassLoader(Collections.emptyList()));
+        CompilationUnitTree compilationUnitTree = parse(codeToParse);
         ClassTree classTree = (ClassTree) compilationUnitTree.types().get(CLASS_INDEX);
         StatementTree statementTree = ((MethodTree) classTree.members().get(CLASS_METHOD_INDEX)).block().body().get(METHOD_INVOCATION_INDEX);
         this.methodInvocationTree = (MethodInvocationTree) ((ExpressionStatementTree) statementTree).expression();
+    }
+
+    private CompilationUnitTree parse(String source) {
+        List<File> classpath = Lists.newArrayList(new File("target/test-classes"), new File("target/classes"));
+        return JParser.parse("1.8", "test", source, classpath);
     }
 
 }
